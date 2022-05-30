3c7621a63edab21fdca6edd9f767ee5d3f9404f1
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-05-30 13:34:11 -0300

    Setup

--- src/main/java/br/com/claro/whatsapp/tracking/TrackingServiceApplication.java
+++ src/main/java/br/com/claro/whatsapp/tracking/TrackingServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "br.com.claro.whatsapp.tracking.config" })
 public class TrackingServiceApplication {
 
 	public static void main(String[] args) {
